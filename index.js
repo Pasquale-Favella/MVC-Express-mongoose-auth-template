33ed04aac59c411666b72edfac52e364bbca4a24
Favella Pasquale <FavellaPa@valueteam.com>
2021-09-08 20:15:30 +0200

    Added some help comments

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 /*FUNCTION TO CONNECT DB*/
 const { connectDB } = require('./db');
 
-/*MIDDLEWARES*/
+/*MIDDLEWARES IMPORT*/
 const ErrorMiddleware = require('./middlewares/error.middleware');
 const AuthMiddleware = require('./middlewares/auth.middleware');
 
-/* ROUTES */
+/* ROUTES IMPORT*/
 const userRoute = require('./routes/user.route');
 const helloWorldRoute = require('./routes/helloworld.route');
 
@@ -16,12 +16,18 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
+/* GLOBAL MIDDLEWARES */
 app.use(express.json());
 app.use(AuthMiddleware.checkTokenSetUser);
 
+/* ROUTES */
 app.use('/auth',userRoute);
+
+/*PROTECTED DUMMY ROUTE AVAILABLE ONLY VIA
+AUTHORIZATION HEADER Bearer token PROVIDED THROUGH LOGIN OR SIGNUP*/
 app.use('/hello',AuthMiddleware.isLoggedIn,helloWorldRoute);
 
+/* ERROR RESPONSE HANDLING */
 app.use(ErrorMiddleware.notFound);
 app.use(ErrorMiddleware.errorHandler);
 
